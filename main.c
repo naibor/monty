79fdc163fcc8d6840067c034d49792dde03b785b
naibor <liznaibor.en@gmail.com>
2022-09-08 00:45:37 +0300

    add global variable and functionality

--- main.c
+++ main.c
@@ -1,20 +1,39 @@
 #include "monty.h"
-/* main - Entry point
+global_t the_global;
+/**
+ * main - Entry point
+ *
  * @argc: number of arguments
  * @argv: list of arguments
+ *
  * Return: returns EXIT_SUCCESS on success else EXIT_FAILURE 
  */
 int main(int argc, char *argv[])
 {
-	instruction_t opcodes[] = {{"push", push_stack}, {"pall", pall}, {"pop", pop}, {"pint", pint},{"add", add}
+	instruction_t opcodes[] = {{"push", push_stack}, {"pall", pall}, {"pop", pop}, {"pint", pint}, {"swap", swap}, {"add", add}
 	};
 	stack_t *stack = NULL;
+	int line_number = 1, getline_res = 0;
+	size_t buf_size = 0;
 
 	if (argc != 2)
 	{	
 		/*return error message*/
 		error_message("USAGE: monty file", "", &stack);
 	}
+	the_global.file = fopen(argv[1], "r");
+	if (!the_global.file)
+		error_message("Error: Can't open file ", argv[1], &stack);
+	while (1)
+	{
+		getline_res = getline(&the_global.Line_buffer, &buf_size, the_global.file);
+		if (getline_res == EOF)
+			break;
+		check_opc(strtok(the_global.Line_buffer, " \t"), &opcodes, line_number,
+			&stack);
+		line_number++;
+	}
+	free_all(&stack);
 	return (EXIT_SUCCESS);
 
 }

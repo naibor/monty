081e632bd5b3d1bf1075b40006eff36e782d15f6
naibor <liznaibor.en@gmail.com>
2022-09-08 01:03:58 +0300

    include get length function

--- swap.c
+++ swap.c
@@ -27,3 +27,21 @@ void swap(stack_t **stack, unsigned int line_number)
 	second->n = first->n;
 	first->n = temp;
 }
+
+/**
+* length_stack - Calculates the length of the stack
+* @head: Head of the stack dlist
+* Return: number of nodes in the stack
+*/
+
+size_t length_stack(const stack_t *head)
+{
+	unsigned int length = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		length++;
+	}
+	return (length);
+}

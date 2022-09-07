11fce201cc1b6383465e73da74a7b7721ab824bf
naibor <liznaibor.en@gmail.com>
2022-09-08 00:10:19 +0300

    refactor error handling function to include stack, create free all function

--- errors.c
+++ errors.c
@@ -8,8 +8,30 @@
 * @arg: argument.
 * @stack: stack to free.
 */
-void error_message(char *message, char *arg)
+void error_message(char *message, char *arg, stack_t **stack)
 {
+	free_all(stack);
 	fprintf(stderr, "%s%s\n", message, arg);
 	exit(EXIT_FAILURE);
+}
+
+/**
+* free_all - Function
+*
+* Description: Frees the stack
+*
+* @stack: stack to free
+* 
+*/
+
+void free_all(stack_t **stack)
+{
+	stack_t *temp;
+
+	while (*stack)
+	{
+		temp = *stack;
+		*stack = (*stack)->next;
+		free(temp);
+	}
 }

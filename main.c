b1d6f622f6e09a158d1afd9496f9f8b119aeb52c
Lewis Safari <lleewwii1@gmail.com>
2022-09-07 17:43:38 -0400

    Fixing Betty Style Issue

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 /* main - Entry point
  * @argc: number of arguments
  * @argv: list of arguments
- * Return: returns EXIT_SUCCESS on success else EXIT_FAILURE 
+ * Return: returns EXIT_SUCCESS on success else EXIT_FAILURE
  */
 int main(int argc, char *argv[])
 {
-	instruction_t opcodes[] = {{"push", push_stack}, {"pall", pall}, {"pop", pop}, {"pint", pint},{"add", add}
+	instruction_t opcodes[] = {{"push", push_stack}, {"pall", pall},
+	 {"pop", pop}, {"pint", pint}, {"add", add}
 	};
 	stack_t *stack = NULL;
 

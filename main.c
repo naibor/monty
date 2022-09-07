a9e247402671b8769fd59fcb62c268b84edab3ad
naibor <liznaibor.en@gmail.com>
2022-09-07 22:09:09 +0300

    initiate entry point of application

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/* main - Entry point
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: returns EXIT_SUCCESS on success else EXIT_FAILURE 
+ */
+int main(int argc, char *argv[])
+{
+	instruction_t opcodes[] = {}
+	};
+	if (argc != 2)
+	{	
+		/*return error message*/
+		error_message("USAGE: monty file", "");
+	}
+	return (EXIT_SUCCESS)
+
+}

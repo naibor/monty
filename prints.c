93175616af0a52aabb8543a82394638ae214d2c4
naibor <liznaibor.en@gmail.com>
2022-09-07 23:25:02 +0300

    create a pint function

--- prints.c
+++ prints.c
@@ -20,3 +20,34 @@ void pall(stack_t **stack, unsigned int line_number)
 		temp = temp->next;
 	}
 }
+
+/**
+* pint - Function
+* Description: Print value at the top of the stack, then a new line
+*
+* @stack: pointer parameter, head of the stack
+* @line_number: line number
+* 
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	const stack_t *temp = *stack;
+	char message[100];
+	unsigned int length_stack = 0;
+
+	/** calculate the length of the stack */
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		length_stack++;
+	}
+	/*check if stack exists or is empty*/
+	if (*stack == NULL || length_stack < 1)
+	{
+		sprintf(message, "L%u: can't pint, stack empty", line_number);
+		error_message(message, "", stack);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
